da952e29d8f574f4f3dda9f9f042b947550a99d8
Ganishk <ganishk1@gmail.com>
2022-10-08 18:12:29 +0530

    updated boilerplate

--- eyrc-2022_holabot/scripts/task_0.py
+++ eyrc-2022_holabot/scripts/task_0.py
@@ -17,22 +17,26 @@
 *****************************************************************************************
 '''
 
-# Team ID:			[ Team-ID ]
-# Author List:		[ Names of team members worked on this file separated by Comma: Name1, Name2, ... ]
+# Team ID:  hb#2938			[ Team-ID ]
+# Author List:	Ganishk D, Srivattan S, Susmitha S, Janam Khandhelwal	[ Names of team members worked on this file separated by Comma: Name1, Name2, ... ]
 # Filename:			task_0.py
-# Functions:
+# Functions: linear, turn, arc
 # 					[ Comma separated list of functions in this file ]
-# Nodes:		    Add your publishing and subscribing node
+# Nodes: /turtlesim, /robot (anonymous)		    Add your publishing and subscribing node
 
 
 ####################### IMPORT MODULES #######################
 import sys
 import traceback
+
+import rospy
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
 ##############################################################
 
 
 def callback(data):
-	"""
+        """
 	Purpose:
 	---
 	This function should be used as a callback. Refer Example #1: Pub-Sub with Custom Message in the Learning Resources Section of the Learning Resources.
@@ -52,10 +56,12 @@ def callback(data):
 	---
         Depends on the usage of the function.
 	"""
+        global pose
+        pose = data
 
 
 def main():
-	"""
+        """
 	Purpose:
 	---
 	This function will be called by the default main function given below.
@@ -73,18 +79,150 @@ def main():
 	---
         main()
 	"""
+        global vel_pub, vel_msg, pose, pose_subscriber, rate
+
+        rospy.init_node('robot',anonymous=True)
+
+        #Create a publisher to send velocity related instructions
+        vel_pub = rospy.Publisher('/turtle1/cmd_vel',Twist, queue_size=10)
+        vel_msg = Twist()
+
+        #Create a susbscriber node to get the pose info
+        pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, callback)
+        pose = Pose()
+
+        rate = rospy.Rate(10000)
+
+######################################
+        try:
+            """
+            Steps involved in drawing D are:
+                1. Draw the semicircular arc of D
+                2. Rotate the bot by 90 degrees so that it points downwards
+                3. Move the bot to the initial position linearly
+            """
+            arc(1,PI)
+            turn(1,PI*0.5)
+            linear(4,2)
+            #rospy.spin()
+
+        except rospy.ROSInterruptException: pass
+        except KeyboardInterrupt: pass
+######################################
+        rospy.signal_shutdown("")
+
+
+
 
 
 ################# ADD GLOBAL VARIABLES HERE #################
 
-
+PI = 3.141592653589793
+DEG2RAD = 0.017453292519943295 #Used to convert degrees into radians
 
 ##############################################################
 
 
 ################# ADD UTILITY FUNCTIONS HERE #################
 
+def linear(vel,dist=None):
+    """
+    To control the linear movement of the turtle
+    @params:
+        Inputs: vel --> velocity to move the bot
+                dist -->distance until which the bot should be moved
+                        ignore to move for infinite length
+        Output: None
+    """
+    print("Moving forward")
+    vel_msg.linear.x    = vel
+    vel_msg.linear.y    = 0
+    vel_msg.linear.z    = 0
+    vel_msg.angular.x   = 0
+    vel_msg.angular.y   = 0
+    vel_msg.angular.z   = 0
 
+    t0 = rospy.Time.now().to_sec()
+    c_dist = 0
+
+    while c_dist < dist: #Run infinitely if dist not specified
+        vel_pub.publish(vel_msg)
+        t1 = rospy.Time.now().to_sec()
+        c_dist = vel*(t1-t0)
+        rate.sleep()
+
+    vel_msg.linear.x    = 0
+    vel_pub.publish(vel_msg)
+
+
+def turn(avel,angle=None):
+    """
+    To control the rotation of the turtle in the plane
+    @params
+    Input:
+        avel --> Angular velocity of the turtle
+        angle -->Angle until which the bot should turn
+    Output:
+        None
+    """
+    print("Turning")
+    vel_msg.linear.x    = 0
+    vel_msg.linear.y    = 0
+    vel_msg.linear.z    = 0
+    vel_msg.angular.x   = 0
+    vel_msg.angular.y   = 0
+    vel_msg.angular.z   = avel
+
+    t0 = rospy.Time.now().to_sec() 
+    c_angle = 0
+
+    while c_angle < angle or not angle: #Run infinitely if angle is not specified
+        vel_pub.publish(vel_msg)
+        t1 = rospy.Time.now().to_sec()
+        c_angle = avel*(t1-t0)
+        rate.sleep()
+
+    vel_msg.angular.z   = 0
+    vel_pub.publish(vel_msg)
+
+
+def arc(vel, angle = None):
+    """
+    To draw the arc of specified angle
+    @params
+    Input:
+        vel --> linear velocity of the bot which is same as angular velocity
+                in unit circular motion.
+    Output:
+        None
+
+    We made it as semi circular arc for unit radius for simplicity
+    not made for any arbitary angles
+    """
+    print("Making an arc")
+    vel_msg.linear.x    = vel
+    vel_msg.linear.y    = 0
+    vel_msg.linear.z    = 0
+    vel_msg.angular.x   = 0
+    vel_msg.angular.y   = 0
+    vel_msg.angular.z   = vel 
+
+
+    print(pose.theta)
+    print("___________")
+
+    t = pose.theta
+
+    while t>=0:
+        vel_pub.publish(vel_msg)
+        t = pose.theta
+        print(pose.x, pose.y, t)
+        #rospy.spin()
+        rate.sleep()
+
+    vel_msg.linear.x    = 0
+    vel_msg.angular.z   = 0
+    vel_pub.publish(vel_msg)
 
 ##############################################################
 
